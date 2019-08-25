3b0739100dbb2d99a6ad0f23ff0d5388ecce4e74
S. D. Mintz <wp710012@gmail.com>
2019-08-25 19:22:18 -0400

    Genesis

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Workspace.ViewModels;
+using System.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Workspace.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            vmHomeIndex model = new vmHomeIndex();
+
+            string s = @"<form style=""padding-top: 50px"" novalidate name=""myForm"" class=""form-inline"" role=""form"" ng-submit=""vm.SendGridAlphaInterested(vm.email.text)"">\r\n
+            <div class=""form-group"" ng-class=""{'has-error' : myForm.input.$invalid && myForm.input.$dirty}"">\r\n
+                @*<label for=""input"" class=""col-sm-3 control-label"">Email</label>*@\r\n
+                <div class=""row"">
+                    <span class=""col-md-4 md-offset-3"">
+                        <input type=""email"" name=""input"" class=""input-lg form-control"" ng-model=""vm.email.text"" ng-required=""true"">
+                    </span>
+                    <span class=""col-md-2"">
+                        <button ng-click='validateEmail()' type=""submit"" class=""btn btn-primary"">Subscribe</button>
+                    </span>
+                    <span class=""col-md-3""></span>
+                </div>
+                <div class=""row"">
+                    <div id=""emErr"" class=""col-md-12"" ng-show=""myForm.input.$invalid && myForm.input.$dirty"" style=""font-size: 20px"">A valid email is required</div>
+                    @*<span ng-show=""myForm.input.$invalid && myForm.input.$dirty "">A valid email is required</span>*@
+                </div>
+            </div>
+        </form>";
+
+            model.HtmlSection = System.Web.HttpUtility.HtmlDecode(s);
+         //   model.HtmlSection = "hello world";
+
+            return View(model);
+        }
+        public IActionResult LearnToClick()
+        {
+            return View();
+        }
+        public IActionResult AvantTekAlpha()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace Workspace
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            //app.UseFileServer(enableDirectoryBrowsing: true);
+            //app.UseDirectoryBrowser();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    //template: "{controller=Workspace}/{action=WorkspaceHello}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+        
+    }
+}
