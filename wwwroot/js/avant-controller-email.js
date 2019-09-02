4a489f4495261a4fb33acbd3689b04e713a521f9
S. D. Mintz <wp710012@gmail.com>
2019-09-02 16:44:25 -0400

    change https cuisine to http

--- wwwroot/js/avant-controller-email.js
+++ wwwroot/js/avant-controller-email.js
@@ -11,7 +11,7 @@ function emailController($scope, $http) {
     };
 
     vm.SendGridAlphaInterested = function(who) {
-            $http.get("https://www.cuisinecuration.com/api/sendgrid/send", { params: { "who": who } })
+            $http.get("http://www.cuisinecuration.com/api/sendgrid/send", { params: { "who": who } })
                 .then(onSendComplete);
         }
 
@@ -32,7 +32,7 @@ function emailController($scope, $http) {
                 return;
 
           // var me = "zozo@ff.noq";
-          $http.get("https://www.cuisinecuration.com/api/sendgrid/send/?", { params: { "who": me } })
+          $http.get("http://www.cuisinecuration.com/api/sendgrid/send/?", { params: { "who": me } })
           // $http.get("http://www.cuisinecuration.com/api/sendgrid/send?who=xx@yy.ets")
                 .then(onSendComplete);
 
