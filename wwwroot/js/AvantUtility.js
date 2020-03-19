51834a297d547817549bcaf6daf2ec61b01ddfff
S. D. Mintz <wp710012@gmail.com>
2020-03-19 13:12:11 -0400

    ToggleStep1OK

--- wwwroot/js/AvantUtility.js
+++ wwwroot/js/AvantUtility.js
@@ -2,8 +2,18 @@ $(document).ready(function () {
     "use strict";
     $('#DevButton').click(function () {
         $('#collapseExample01').collapse('toggle');
+        $('#collapseExample02').collapse('hide');
+        $('#collapseExample03').collapse('hide');
     });
     $('#InfoButton').click(function () {
         $('#collapseExample02').collapse('toggle');
+        $('#collapseExample01').collapse('hide');
+        $('#collapseExample03').collapse('hide');
+        
+    });
+    $('#CloudButton').click(function () {
+        $('#collapseExample03').collapse('toggle');
+        $('#collapseExample02').collapse('hide');
+        $('#collapseExample01').collapse('hide');
     });
 }); 
