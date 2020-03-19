81418f693d66536850583193b602832d4f009b1b
S. D. Mintz <wp710012@gmail.com>
2020-03-19 12:37:17 -0400

    ShowHideTested!

--- wwwroot/js/AvantUtility.js
+++ wwwroot/js/AvantUtility.js
@@ -1,12 +1,9 @@
-$(document).ready(function() {
-  "use strict";
- /*  $("#DevButton").click(function(){
-    alert("The button was clicked.");
-  }); */
-  $('#DevButton').click(function () {
-    $('#collapseExample01').collapse('toggle');
-``});
-$('InfoButton').click(function() {
-  $('#collapseExample02').collapse('toggle');
-});
-});
+$(document).ready(function () {
+    "use strict";
+    $('#DevButton').click(function () {
+        $('#collapseExample01').collapse('toggle');
+    });
+    $('#InfoButton').click(function () {
+        $('#collapseExample02').collapse('toggle');
+    });
+}); 
